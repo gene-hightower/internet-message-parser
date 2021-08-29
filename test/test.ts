a0778ef8c4c02630205348a5773d83e41d5895a3
Gene Hightower <gene@digilicious.com>
2021-08-29 11:29:50 -0700

    actual test

--- test/test.ts
+++ test/test.ts
@@ -5,7 +5,8 @@ const assert = require("assert");
 const { parse } = require("../lib/index.js");
 
 describe("try anything", function () {
-    it("", function () {
-        console.log(JSON.stringify(parse('atom', 'foo')));
+    it("parse an atom", function () {
+        const atom = parse('atom', 'foo');
+        expect(atom).toEqual({atom: 'foo'});
     });
 });
