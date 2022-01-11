7e7458337bb2410aa524aa6c8e3e8e1613e36682
Gene Hightower <gene@digilicious.com>
2022-01-11 10:29:21 -0800

    1st test

---
+++ test/message.test.ts
@@ -0,0 +1,8 @@
+import { Message, is_structured_header } from "../lib/Message";
+
+describe("a test", () => {
+  it("this should pass", () => {
+    const pass = "pass";
+    expect(pass).toStrictEqual("pass");
+  });
+});
