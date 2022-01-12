04749daf2ee2eca314ff9b426cfc1af38f65d25d
Gene Hightower <gene@digilicious.com>
2022-01-11 19:56:24 -0800

    test exceptions

---
+++ test/exceptions.test.ts
@@ -0,0 +1,25 @@
+import dedent from "ts-dedent";
+
+import { Message } from "../lib/Message";
+
+describe("Parse failures", () => {
+  it("bad Date: syntax", () => {
+    const msg_text = Buffer.from(
+      dedent`
+        Date:     26 Aug 76 1429 EDT (missing ':' between hours and minutes)
+        From:     Jones@Registry.Org
+        Bcc:
+    `.replace(/\n/g, "\r\n") + "\r\n"
+    ); // CRLF line endings
+
+    try {
+      const msg = new Message(msg_text);
+    } catch (e) {
+      const ex = e as Error;
+      expect(ex.message).toEqual('syntax error in Date: header');
+    }
+
+    expect(false);
+  });
+
+});
