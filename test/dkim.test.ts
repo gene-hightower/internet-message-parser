a66cb78152540bd9466a833cff3c6a474c06b5dc
Gene Hightower <gene@digilicious.com>
2022-01-17 17:17:56 -0800

    less ugly

--- test/dkim.test.ts
+++ test/dkim.test.ts
@@ -4,8 +4,7 @@ import { Message, MessageType } from "../lib/Message";
 
 describe("DKIM Signatures RFC-6376 ", () => {
   it("Simple parse test", () => {
-    const msg_text = Buffer.from(
-`DKIM-Signature: v=1; a=rsa-sha256; c=relaxed/relaxed;
+    const msg_text = Buffer.from(`DKIM-Signature: v=1; a=rsa-sha256; c=relaxed/relaxed;
         d=gmail.com; s=20161025;
         h=mime-version:reply-to:from:date:message-id:subject:to;
         bh=8RegOlJD0udPc+TogIjhWcR2b9VwF9rz0+RfjmeNupQ=;
@@ -14,8 +13,7 @@ describe("DKIM Signatures RFC-6376 ", () => {
          50NfZ3BQb6EIE/BvNoYtmc/qspPdlcKM0mHAS8XcBfnSrk62V708HRNDNaVKpSQ6fzyH
          9niRDzpV7Db89ouUx2jGXzZzAjmQmoajHUN0dD8Q7UcEdELsSSTkjHq2QR7yijW6fOEh
          Divt3vAssuiMVsvsvErsy7RPM5ImfTg8L8C38GRpDjiy7h8g2J3cQkwnrHEwWXPzaocB
-         KGoQ==
-`.replace(/\n/g, "\r\n")
+         KGoQ==`.replace(/\n/g, "\r\n") + "\r\n"
     ); // CRLF line endings
 
     const msg = new Message(msg_text, MessageType.part);
